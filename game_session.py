d13ad6c137e798552637f60bccec5be4bfab85e3
Pau Piñol <pau.pinol@estudiants.urv.cat>
2025-04-12 00:52:08 +0200

    Add GameSession library

--- game_session.py
+++ game_session.py
@@ -53,19 +53,3 @@ class GameSession:
         data = response.json()
         self.client_id = data.get("client_id", self.client_id)
         return data
-
-def main():
-    API_KEY = "BAh5a5BH_zic0UbuFm8pqlHRcc_ctl1DuYmcDl8-yok" 
-    game = GameSession(api_key=API_KEY, player_name="QuackCoders")
-
-    game.start_game()
-
-    while True:
-        result = game.make_decision(decision="Accept")
-        filtered_result = {k: v for k, v in result.items() if k != "client_data"}
-        print(filtered_result)
-        if result.get("status") == "gameover":
-            break
-
-if __name__ == "__main__":
-    main()
