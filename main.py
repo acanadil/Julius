810ac6f1e2742eb08ae53bceeaa72947ddb9a7e7
Pau Piñol <pau.pinol@estudiants.urv.cat>
2025-04-12 04:05:42 +0200

    Implement saving decisions

--- main.py
+++ main.py
@@ -1,17 +1,37 @@
+import requests
 from game_session import GameSession
 
 def main():
-    API_KEY = "BAh5a5BH_zic0UbuFm8pqlHRcc_ctl1DuYmcDl8-yok" 
-    game = GameSession(api_key=API_KEY, player_name="QuackCoders")
-
-    game.start_game()
-
+    API_KEY = "BAh5a5BH_zic0UbuFm8pqlHRcc_ctl1DuYmcDl8-yok"
+    player_name = "QuackCoders"
+    
+    last_result = None
+    
     while True:
-        result = game.make_decision(decision="Accept")
-        filtered_result = {k: v for k, v in result.items() if k != "client_data"}
-        print(filtered_result)
-        if result.get("status") == "gameover":
-            break
+        game = GameSession(api_key=API_KEY, player_name=player_name)
+        game.start_game()
+        
+        result = None
+        while True:
+            result = game.make_decision(decision="Accept")
+            filtered_result = {k: v for k, v in result.items() if k != "client_data"}
+            print(filtered_result)
+
+            post_url = f"http://172.16.206.75:4160/add_files?outcome={result.get("status")}"
+            try:
+                result_to_post = last_result if last_result is not None else result
+                post_response = requests.post(post_url, json=result_to_post)
+                print("POST a last_result:", post_response.status_code, post_response.text)
+            except Exception as e:
+                print("Error al enviar last_result:", e)
+            
+            last_result = result
+            
+            if result.get("status") == "gameover":
+                break
+        
+
+        print("\nReiniciando partida...\n")
 
 if __name__ == "__main__":
     main()
